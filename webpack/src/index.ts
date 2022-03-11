d65409a985f23b01eecb27d51aa838cc58ff524b
Thitat Auareesuksakul <thitat@flux.ci>
2022-03-12 01:32:40 +0700

    feat: add fontawesome icons

--- webpack/src/index.ts
+++ webpack/src/index.ts
@@ -2,3 +2,10 @@ import "bootstrap";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/index.scss";
+
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faExclamationTriangle);
+
+dom.watch();
