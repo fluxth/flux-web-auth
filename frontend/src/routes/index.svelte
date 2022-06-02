2b1a37cda35c0540b276649b661e1a8d524d05ae
Thitat Auareesuksakul <thitat@flux.ci>
2022-06-02 22:24:02 +0700

    feat(frontend): add continue and back url to index page

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
-  import Card from '../components/Card.svelte';
-  import HeaderIcon from 'eva-icons/outline/svg/person-delete-outline.svg';
+  import Card from "../components/Card.svelte";
+  import HeaderIcon from "eva-icons/outline/svg/person-delete-outline.svg";
+  import { page } from "$app/stores";
+
+  $: nextUrl = encodeURIComponent($page.url.href);
 </script>
 
 <Card>
   <div class="text-center mb-6">
     <HeaderIcon width="50" class="mx-auto mb-4 fill-stone-400" />
     <h1 class="text-xl font-medium">You are not logged in</h1>
-    <p class="text-stone-500 dark:text-stone-400 font-light">
-      No active session detected
-    </p>
+    <p class="text-stone-500 dark:text-stone-400 font-light">No active session detected</p>
   </div>
   <div class="text-sm text-center">
-    <a href="/login?continue=a" class="block px-6 py-2 text-white bg-teal-700 shadow hover:shadow-lg hover:bg-teal-800 active:bg-teal-900 rounded-md transition w-full">
+    <a
+      href="/login?continue={nextUrl}&back={nextUrl}"
+      class="block px-6 py-2 text-white bg-teal-700 shadow hover:shadow-lg hover:bg-teal-800 active:bg-teal-900 rounded-md transition w-full"
+    >
       Sign in
     </a>
   </div>
