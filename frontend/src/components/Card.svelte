e8c2ffea83f53646aace820e305e1478b3a0937a
Thitat Auareesuksakul <thitat@flux.ci>
2022-06-04 16:41:27 +0700

    feat(frontend/Card): add noscript alert message

--- frontend/src/components/Card.svelte
+++ frontend/src/components/Card.svelte
@@ -1,5 +1,15 @@
+<script lang="ts">
+  import AlertIcon from "eva-icons/outline/svg/alert-circle-outline.svg";
+</script>
+
 <div class="sm:w-96 mx-auto">
   <div class="flex flex-col h-screen justify-center px-4 gap-2">
+    <noscript>
+      <div class="flex items-center gap-2 py-3 px-4 bg-red-200 rounded text-red-800 text-sm">
+        <AlertIcon width="20" class="inline fill-red-800" />
+        Please enable JavaScript to run this app!
+      </div>
+    </noscript>
     <div
       class="relative overflow-clip p-8 border border-stone-200 dark:border-stone-700 shadow-lg rounded-md dark:bg-stone-900"
     >
