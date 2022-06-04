58fa1114e1fcf4681fb6ce418e90efe5014badba
Thitat Auareesuksakul <thitat@flux.ci>
2022-06-04 16:40:17 +0700

    fix(frontend/Card): make Card layout able to have surrounding elements

--- frontend/src/components/Card.svelte
+++ frontend/src/components/Card.svelte
@@ -1,5 +1,9 @@
-<div class="flex flex-col h-screen justify-center px-4">
-  <div class="relative overflow-clip p-8 w-full sm:w-96 mx-auto border border-stone-200 dark:border-stone-700 shadow-lg rounded-md dark:bg-stone-900">
-    <slot />
+<div class="sm:w-96 mx-auto">
+  <div class="flex flex-col h-screen justify-center px-4 gap-2">
+    <div
+      class="relative overflow-clip p-8 border border-stone-200 dark:border-stone-700 shadow-lg rounded-md dark:bg-stone-900"
+    >
+      <slot />
+    </div>
   </div>
 </div>
