00430f2c3dc69c0b86ddc734a389df1453146a00
Thitat Auareesuksakul <thitat@flux.ci>
2022-06-04 19:56:49 +0700

    feat(frontend/Card): add copyright and links to a footer

--- frontend/src/components/Card.svelte
+++ frontend/src/components/Card.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
   import AlertIcon from "eva-icons/outline/svg/alert-circle-outline.svg";
+  import { page } from "$app/stores";
+
+  export let showHelp: boolean = true;
+  $: pageUrl = encodeURIComponent($page.url.href);
 </script>
 
 <div class="sm:w-96 mx-auto">
@@ -14,6 +18,14 @@
       class="relative overflow-clip p-8 border border-stone-200 dark:border-stone-700 shadow-lg rounded-md dark:bg-stone-900"
     >
       <slot />
+    </div>
+    <div class="px-1 flex justify-between items-center text-stone-400 font-light text-[0.6rem]">
+      <span>&copy; 2022, flux.ci</span>
+      <div class="flex gap-2 underline">
+        {#if showHelp}
+          <a href="/help?back={pageUrl}">What's this?</a>
+        {/if}
+      </div>
     </div>
   </div>
 </div>
