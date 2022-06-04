f7b9eaec3132c7d2faa3b88c9d75cc020dafe021
Thitat Auareesuksakul <thitat@flux.ci>
2022-06-05 01:40:33 +0700

    feat(backend/config): add ALLOWED_URLS configuration

--- backend/src/config.rs
+++ backend/src/config.rs
@@ -1,4 +1,25 @@
 use serde::Deserialize;
+use std::ops::Deref;
+
+#[derive(Debug)]
+pub struct Url(url::Url);
+
+impl Deref for Url {
+    type Target = url::Url;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<'de> Deserialize<'de> for Url {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let string = String::deserialize(deserializer)?;
+        Ok(Url(url::Url::parse(&string).unwrap()))
+    }
+}
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
@@ -6,6 +27,7 @@ pub struct Config {
     pub host: String,
     #[serde(default = "default_port")]
     pub port: u16,
+    pub allowed_urls: Vec<Url>,
 }
 
 fn default_host() -> String {
