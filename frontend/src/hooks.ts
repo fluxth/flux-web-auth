078cbd8fa5cb02e3ed26d12d2c83136044965e60
Thitat Auareesuksakul <thitat@flux.ci>
2022-06-20 23:31:42 +0700

    feat(frontend): try decoding jwt before sending to backend
    This doesn't check the validity of the signature yet! That is left
    for the backend. This only checks if the JWT successfully decodes,
    and if the "iss" is "auth.flux.ci"

--- frontend/src/hooks.ts
+++ frontend/src/hooks.ts
@@ -1,13 +1,38 @@
 import type { GetSession, Handle } from "@sveltejs/kit";
-import { parse } from "cookie";
+import { parse, serialize } from "cookie";
+import { decode } from "jsonwebtoken";
+
+const AUTH_COOKIE_NAME = import.meta.env.VITE_AUTH_COOKIE_NAME;
+const AUTH_COOKIE_DOMAIN = import.meta.env.VITE_AUTH_COOKIE_DOMAIN;
+const AUTH_TOKEN_ISSUER = "auth.flux.ci";
 
 export const handle: Handle = async ({ resolve, event }) => {
+  let clearAuthCookie = false;
+
   const cookies = parse(event.request.headers.get("cookie") || "");
-  if (cookies.authtoken) {
-    event.locals.authtoken = cookies.authtoken;
+  if (cookies[AUTH_COOKIE_NAME]) {
+    const token = cookies[AUTH_COOKIE_NAME];
+    const decoded = decode(token, { json: true });
+
+    // Try check if JWT is decodable, and if the issuer is this app.
+    // This *doesn't* verify the JWT signature!
+    if (decoded?.iss === AUTH_TOKEN_ISSUER) event.locals.authtoken = token;
+    else clearAuthCookie = true;
   }
 
   const response = await resolve(event);
+
+  if (clearAuthCookie) {
+    response.headers.append(
+      "Set-Cookie",
+      serialize(AUTH_COOKIE_NAME, "", {
+        domain: AUTH_COOKIE_DOMAIN,
+        path: "/",
+        expires: new Date(0),
+      })
+    );
+  }
+
   return response;
 };
 
