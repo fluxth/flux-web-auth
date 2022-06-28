d5096451f34686824d256ddbafcbcabafe82fc18
Thitat Auareesuksakul <thitat@flux.ci>
2022-06-28 23:41:01 +0700

    feat(backend): add jwt related utility functions

--- backend/src/utils.rs
+++ backend/src/utils.rs
@@ -1,7 +1,13 @@
-use crate::TOKEN_KEY;
+use anyhow::Result;
+use jwt::{PKeyWithDigest, VerifyWithKey};
+use openssl::pkey::Public;
+use serde::{Deserialize, Serialize};
+use std::borrow::Cow;
 use tonic::Request;
 
-pub fn get_token<T>(request: &Request<T>) -> Option<String> {
+use crate::TOKEN_KEY;
+
+pub fn get_token<T>(request: &Request<T>) -> Option<Cow<str>> {
     if let Some(token) = request.metadata().get(TOKEN_KEY) {
         if let Ok(token_str) = token.to_str() {
             Some(token_str.into())
@@ -11,4 +17,23 @@ pub fn get_token<T>(request: &Request<T>) -> Option<String> {
     } else {
         None
     }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Claims {
+    pub iss: String,
+    pub sub: String,
+    pub exp: u64,
+    pub iat: u64,
+    pub nbf: u64,
+}
+
+type JwtToken = jwt::Token<jwt::Header, Claims, jwt::Verified>;
+pub fn verify_jwt(key: &PKeyWithDigest<Public>, jwt: &str) -> Result<JwtToken> {
+    Ok(VerifyWithKey::verify_with_key(jwt, key)?)
+}
+
+pub fn has_valid_session(_jwt: &JwtToken) -> Result<bool> {
+    // FIXME: validate session
+    Ok(true)
 }
