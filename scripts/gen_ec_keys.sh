ac3a2374abaaa25739b0cd19f65cef09eff4e322
Thitat Auareesuksakul <thitat@flux.ci>
2022-06-28 22:43:51 +0700

    scripts: also generate pcks8 version of private key in gen_ec_keys.sh

--- scripts/gen_ec_keys.sh
+++ scripts/gen_ec_keys.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-PRIVATE_KEY_FILE=private_key.pem
-PUBLIC_KEY_FILE=public_key.pem
+PRIVATE_KEY_FILE=private_key
+PUBLIC_KEY_FILE=public_key
 
-openssl ecparam -genkey -name secp256r1 -noout -out $PRIVATE_KEY_FILE
-openssl ec -in $PRIVATE_KEY_FILE -pubout > $PUBLIC_KEY_FILE
+openssl ecparam -genkey -name prime256v1 -noout -out ${PRIVATE_KEY_FILE}.pem
+openssl ec -in ${PRIVATE_KEY_FILE}.pem -pubout > ${PUBLIC_KEY_FILE}.pem
+
+openssl pkcs8 -nocrypt -topk8 -in ${PRIVATE_KEY_FILE}.pem -out ${PRIVATE_KEY_FILE}.pkcs8.pem
